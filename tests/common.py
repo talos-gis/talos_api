37efa70031679310b439ac926b63b0d3818ffa88
Idan Miara <idan@miara.com>
2020-08-17 15:10:33 +0300

    common.py - reformat and remove some unnecessary code

--- tests/common.py
+++ tests/common.py
@@ -1,8 +1,10 @@
 import lxml.etree as etree
 import sys
 import urllib
+
 PY2 = sys.version_info[0] == 2
 
+# server_wps_url, server_base_url are used in the tests
 from app_config import server_wps_url, server_base_url
 
 NAMESPACES = {
@@ -17,30 +19,29 @@ NAMESPACES = {
 if not PY2:
     import urllib.request
 
+
 def get_response(url, post_data=None):
-
-    response = None
-
     if PY2:
         response = urllib.urlopen(url, data=post_data)
     else:
-        #if post_data:
+        # if post_data:
         #    post_data = post_data.decode()
         response = urllib.request.urlopen(url, data=post_data)
 
     return response
 
-def get_schema(url):
 
+def get_schema(url):
     schema = get_response(url)
     xmlschema_doc = etree.parse(schema)
     return etree.XMLSchema(xmlschema_doc)
 
-def validate_file(path, schema):
 
+def validate_file(path, schema):
     body_doc = etree.parse(path)
     schema = get_schema(schema)
     return schema.validate(body_doc)
+
 
 def validate(url, schema, post_data=None):
     response = get_response(url, post_data)
@@ -53,6 +54,6 @@ def validate(url, schema, post_data=None):
     if schema.validate(body_doc):
         return True
     else:
+        print(info)
         print(body)
         return False
-
