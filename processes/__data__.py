566a34b96489eb1fcb26056241e4abbc15f2fe0e
Idan Miara <idan@miara.com>
2021-02-04 15:25:25 +0200

    v2.2.2

--- processes/__data__.py
+++ processes/__data__.py
@@ -1,4 +1,6 @@
-__version__ = "2.2.0"
+import os
+
+__version__ = open(os.path.join(os.path.dirname(__file__), '../VERSION.txt')).read().rstrip()
 __author__ = "Idan Miara"
 __author_email__ = "idan@miara.com"
 __license__ = "MIT"
