656faf09e621e677bc18dc9b14fcbb1ee87de4e7
Idan Miara <idan@miara.com>
2021-03-15 21:03:02 +0200

    processes/xyz.py - add band categories to the output

--- processes/xyz.py
+++ processes/xyz.py
@@ -1,3 +1,5 @@
+from osgeo import gdal
+
 import processes.io_generator as iog
 from gdalos.gdal2xyz import gdal2xyz
 from processes import process_helper
@@ -18,7 +20,7 @@ class XYZ(Process):
             iog.skip_src_dst_nodata(defaults)
 
         outputs = iog.output_r() + \
-                  iog.output_value(['x', 'y', 'z', 'nodata'])
+                  iog.output_value(['x', 'y', 'z', 'nodata', 'categories'])
 
         super().__init__(
             self._handler,
@@ -39,19 +41,26 @@ class XYZ(Process):
         skip_nodata = process_helper.get_request_data(request.inputs, 'skip_nodata')
         src_nodata = process_helper.get_request_data(request.inputs, 'src_nodata')
         dst_nodata = process_helper.get_request_data(request.inputs, 'dst_nodata')
-        x, y, z, nodata = gdal2xyz(raster_filename, None, return_np_arrays=True,
-                                   skip_nodata=skip_nodata, src_nodata=src_nodata, dst_nodata=dst_nodata)
-
         response.outputs['r'].data = raster_filename
 
-        response.outputs['nodata'].data = nodata
+        ds = gdal.Open(str(raster_filename))
+        outputs = gdal2xyz(
+            ds, None, return_np_arrays=True,
+            skip_nodata=skip_nodata, src_nodata=src_nodata, dst_nodata=dst_nodata)
+        if outputs is not None:
+            x, y, z, nodata = outputs
+            bnd = ds.GetRasterBand(1)
+            categories = bnd.GetCategoryNames()
 
-        response.outputs['x'].output_format = FORMATS.JSON
-        response.outputs['y'].output_format = FORMATS.JSON
-        response.outputs['z'].output_format = FORMATS.JSON
+            response.outputs['nodata'].data = nodata
 
-        response.outputs['x'].data = x
-        response.outputs['y'].data = y
-        response.outputs['z'].data = z
+            response.outputs['x'].output_format = FORMATS.JSON
+            response.outputs['y'].output_format = FORMATS.JSON
+            response.outputs['z'].output_format = FORMATS.JSON
+
+            response.outputs['x'].data = x
+            response.outputs['y'].data = y
+            response.outputs['z'].data = z
+            response.outputs['categories'].data = categories
 
         return response
